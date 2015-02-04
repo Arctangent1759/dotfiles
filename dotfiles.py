26a822ef8dc09cb1b72a2cd7ad681bc0a851215f
Alexander Chu <alex.p.chu@gmail.com>
2015-02-04 11:01:21 -0800

    edit

--- dotfiles.py
+++ dotfiles.py
@@ -1,5 +1,6 @@
 from json import loads
 from os import path
+from sys import argv
 import os
 import shutil
 
@@ -29,10 +30,36 @@ class Dotfiles:
     def install(self):
         if path.exists(path.expanduser(self.conf.dotfile_dir)):
             return False
+        return self.update()
+    def update(self):
+        if path.exists(path.expanduser(self.conf.dotfile_dir)):
+            shutil.rmtree(path.expanduser(self.conf.dotfile_dir))
         for conf in self.conf.conf:
             for symlink in conf.symlinks:
+                os.remove(path.expanduser(symlink.to_loc))
                 os.symlink(
                     path.expanduser(symlink.from_loc),
                     path.expanduser(symlink.to_loc))
         shutil.copytree("dotfiles", path.expanduser(self.conf.dotfile_dir))
         return True
+    def pull(self):
+        if not path.exists(path.expanduser(self.conf.dotfile_dir)):
+            return False
+        shutil.rmtree("dotfiles")
+        shutil.copytree(path.expanduser(self.conf.dotfile_dir),"dotfiles")
+        return True
+
+if __name__ == '__main__':
+    d = Dotfiles()
+    if (argv[1] == 'install'):
+        print 'Success' if d.install() else 'Failure'
+    elif (argv[1] == 'update'):
+        print 'Success' if d.update() else 'Failure'
+    elif (argv[1] == 'pull'):
+        print 'Success' if d.pull() else 'Failure'
+    else:
+        print """
+        install -- install dotfiles.
+        update  -- update dotfiles.
+        pull    -- fetch dotfile updates.
+        """
